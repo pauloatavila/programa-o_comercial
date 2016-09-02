88bb33a7e416bf7b6f5f9cc4517e2c68c8528415
felipermb <felipe_rdmd@hotmail.com>
2016-09-02 20:00:12 -0300

    Tutorial corrigido

--- Site Aula/README.md
+++
@@ -1,7 +0,0 @@
-﻿# programacao_comercial
-Disciplina de Programação Comercial
-
-teste commit
-
-
-https://docs.djangoproject.com/pt-br/1.10/intro/
--- Site Aula/virtual/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/aluno/Aluno/programa-o_comercial/virtual/bin/python2
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
